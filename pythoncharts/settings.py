4511f821efac936ced5ef38f6c46dc7894861737
mvsrakeshkumarreddy <mvsrakeshkumarreddy@gmail.com>
2022-12-27 15:34:56 +0530

    firstd

--- pythoncharts/settings.py
+++ pythoncharts/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = 'django-insecure-4zp9l#qndnmsxiz+qhd37*lu24q63p94a#@zy-ny_1#m2#bl*x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','https://web-production-402f.up.railway.app', 'http://www.spmreportautomation.com']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-402f.up.railway.app','http://www.spmreportautomation.com']
+ALLOWED_HOSTS = ['*','*.spmreportautomation.com','www.spmreportautomation.com','http://spmreportautomation.com','https://web-production-402f.up.railway.app', 'http://www.spmreportautomation.com']
+CSRF_TRUSTED_ORIGINS = ['*','*.spmreportautomation.com','www.spmreportautomation.com','http://spmreportautomation.com','https://web-production-402f.up.railway.app','http://www.spmreportautomation.com']
 
 
 #AUTH_USER_MODEL = 'auth.User'
